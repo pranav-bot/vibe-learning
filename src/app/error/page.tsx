457847800a3733a50414140a6cb9e8d6c10a21d0
pranav <pranav23advani@gmail.com>
2025-06-22 14:53:39 +0530

    deployment fixes

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import ThemeToggle from "~/components/ThemeToggle";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Button } from "~/components/ui/button";
